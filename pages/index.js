2d0c9cf149b866ce434ddae9ac292b9049ccfdfa
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:53:02 +0100

    d
    d

--- pages/index.js
+++ pages/index.js
@@ -23,11 +23,11 @@ export default function Home() {
         </ol>
       </main>
       <div className="members">
-      <img src="https://cdn.discordapp.com/avatars/500649100316901376/a_e1097fd6d54d0b697e5f5838f3ec3f8c.gif?size=4096" width ="50" height="50" alt="Epic Vibe"></img>
-            <img src="https://cdn.discordapp.com/avatars/526090380471697440/d598b819c98757d54bb803718a0abb03.png?size=4096" width ="50" height="50" alt="Phuyeso"></img>
-            <img src="https://cdn.discordapp.com/avatars/758695634567430154/8c7b467486efd1964c213bad5a53c179.png?size=4096" width ="50" height="50" alt="Itamis"></img>
-            <img src="https://cdn.discordapp.com/avatars/897152552422998026/4a35bd089a6709dea97373107f4badb5.png?size=4096" width ="50" height="50" alt="Emexure"></img>
-            <img src="https://cdn.discordapp.com/avatars/736196841095299102/373e77cf873c7ce8d2d92f7d1ac28fd7.png?size=4096" width ="50" height="50" alt="Jess"></img>
+      <img src="https://cdn.discordapp.com/avatars/500649100316901376/a_e1097fd6d54d0b697e5f5838f3ec3f8c.gif?size=4096" width ="150" height="150" alt="Epic Vibe"></img>
+            <img src="https://cdn.discordapp.com/avatars/526090380471697440/d598b819c98757d54bb803718a0abb03.png?size=4096" width ="150" height="150" alt="Phuyeso"></img>
+            <img src="https://cdn.discordapp.com/avatars/758695634567430154/8c7b467486efd1964c213bad5a53c179.png?size=4096" width ="150" height="150" alt="Itamis"></img>
+            <img src="https://cdn.discordapp.com/avatars/897152552422998026/4a35bd089a6709dea97373107f4badb5.png?size=4096" width ="150" height="150" alt="Emexure"></img>
+            <img src="https://cdn.discordapp.com/avatars/736196841095299102/373e77cf873c7ce8d2d92f7d1ac28fd7.png?size=4096" width ="150" height="150" alt="Jess"></img>
 </div>
       <Footer />
       </div>
