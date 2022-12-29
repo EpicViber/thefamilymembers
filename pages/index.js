628eac4b052d7167ace8065a65ecb9e69a1c7ab3
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:28:19 +0100

    f
    d

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,14 @@ export default function Home() {
         <p className="description">
           <code>A group owned by a few idiots on discord</code>
         </p>
-        <img src="https://cdn.discordapp.com/attachments/565187025058136075/1058000579101732934/DALLE_2022-10-03_18.55.19_-_only_in_ohio.png" alt="group icon" width="450" height="450"></img>
       </main>
+      <div className="members">
+      <img src="https://cdn.discordapp.com/avatars/500649100316901376/a_e1097fd6d54d0b697e5f5838f3ec3f8c.gif?size=4096" width ="350" height="350" alt="Epic Vibe"></img>
+            <img src="https://cdn.discordapp.com/avatars/526090380471697440/d598b819c98757d54bb803718a0abb03.png?size=4096" width ="350" height="350" alt="Phuyeso"></img>
+            <img src="https://cdn.discordapp.com/avatars/758695634567430154/8c7b467486efd1964c213bad5a53c179.png?size=4096" width ="350" height="350" alt="Itamis"></img>
+            <img src="https://cdn.discordapp.com/avatars/897152552422998026/4a35bd089a6709dea97373107f4badb5.png?size=4096" width ="350" height="350" alt="Emexure"></img>
+            <img src="https://cdn.discordapp.com/avatars/736196841095299102/373e77cf873c7ce8d2d92f7d1ac28fd7.png?size=4096" width ="350" height="350" alt="Jess"></img>
+</div>
       <Footer />
       </div>
   )
