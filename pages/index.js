95fdad2a11f75ef4be32ab5a541a1f1416c5d5d7
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 14:06:58 +0100

    yes
    yes

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>The Family</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="THE FAMILY" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          A group owned by a few idiots on <code>discord</code>
         </p>
+
+        <img src="https://cdn.discordapp.com/avatars/500649100316901376/a_e1097fd6d54d0b697e5f5838f3ec3f8c.gif?size=4096" alt="Epic Vibe"></img>
       </main>
 
       <Footer />
