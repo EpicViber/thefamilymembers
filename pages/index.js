bae0825f0781012e3fa9609c0a97fc87b45b5b19
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:05:57 +0100

    d
    d

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <p className="description">
           <code>A group owned by a few idiots on discord</code>
         </p>
-        <img src="https://cdn.discordapp.com/attachments/565187025058136075/1058000579101732934/DALLE_2022-10-03_18.55.19_-_only_in_ohio.png"></img>
+        <img src="https://cdn.discordapp.com/attachments/565187025058136075/1058000579101732934/DALLE_2022-10-03_18.55.19_-_only_in_ohio.png" alt="group icon" width="300" height="300"></img>
       </main>
       <Footer />
       </div>
