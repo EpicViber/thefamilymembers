4c3cd0afa49f072dacb76fad1cd7a5dea76eba31
EpicVibe <58629230+EpicViber@users.noreply.github.com>
2022-12-29 17:00:53 +0100

    d
    d

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,7 @@ export default function Home() {
         
       </main>
       <div className="members">
-      <img src="https://cdn.discordapp.com/avatars/500649100316901376/a_e1097fd6d54d0b697e5f5838f3ec3f8c.gif?size=4096" alt="Epic Vibe"></img>
-            <img src="https://cdn.discordapp.com/avatars/526090380471697440/d598b819c98757d54bb803718a0abb03.png?size=4096"></img>
-            <img src="https://cdn.discordapp.com/avatars/758695634567430154/8c7b467486efd1964c213bad5a53c179.png?size=4096"></img>
-            <img src="https://cdn.discordapp.com/avatars/897152552422998026/4a35bd089a6709dea97373107f4badb5.png?size=4096"></img>
-            <img src="https://cdn.discordapp.com/attachments/1001371494133276682/1057752677343314020/20221228_200502.jpg"></img>
+      <img src="https://cdn.discordapp.com/attachments/565187025058136075/1058000579101732934/DALLE_2022-10-03_18.55.19_-_only_in_ohio.png"></img>
       </div>
       <Footer />
       </div>
